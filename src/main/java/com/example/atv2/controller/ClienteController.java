5a0d07ecbc024d70b40659c4e6d89d5e3511ea21
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2026-04-08 15:56:47 -0300

    Feat: Implementa ResponseEntity(Para estabelecer status nas requisições)

--- src/main/java/com/example/atv2/controller/ClienteController.java
+++ src/main/java/com/example/atv2/controller/ClienteController.java
@@ -4,6 +4,8 @@ import com.example.atv2.dto.Cliente.ClienteRequest;
 import com.example.atv2.dto.Cliente.ClienteResponse;
 import com.example.atv2.service.ClienteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +18,21 @@ public class ClienteController {
     private final ClienteService service;
 
     @PostMapping("add")
-    public ClienteResponse create(@RequestBody  ClienteRequest dto){
-        return service.create(dto);
+    public ResponseEntity<ClienteResponse> create(@RequestBody  ClienteRequest dto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.create(dto));
     }
 
     @GetMapping("listAll")
-    public List<ClienteResponse> listAll(){
-        return service.listAll();
+    public ResponseEntity<List<ClienteResponse>> listAll(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.listAll());
     }
 
     @GetMapping("list/{id}")
-    public ClienteResponse listById(@PathVariable long id){
-        return service.listById(id);
+    public ResponseEntity<ClienteResponse> listById(@PathVariable long id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.listById(id));
     }
 
 }
